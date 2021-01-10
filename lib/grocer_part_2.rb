44647a074ccd5bbda1d07f295d1345944c5d449d
Blaine R Love <blainelove94@gmail.com>
2021-01-10 19:26:23 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,18 +1,73 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
+  binding.pry
+  receipt = []
+  if coupons == []
+    return cart
+  end
+  cart.each do |product|
+    coupons.each do |discounted|
+      if product[:item] == discounted[:item]
+        qty = product[:count]
+        remain = product[:count] % discounted[:num]
+
+        product[:count] = remain
+        receipt << product
+        binding.pry
+        couponItemHash = {:item => product[:item] + ' W/COUPON',
+                         :price => discounted[:cost] / discounted[:num],
+                         :clearance => product[:clearance],
+                         :count => qty - remain}
+        receipt << couponItemHash
+        binding.pry
+      else
+        receipt << product
+      end
+    end
+  end
+  return receipt
+end
+
+
+
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
+
 
 def apply_clearance(cart)
+  cart.each do |product|
+    product[:price] = product[:price] * 0.80 if product[:clearance]
+  end
+end
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
+
 
 def checkout(cart, coupons)
+  binding.pry
+  total = 0
+  conCart = consolidate_cart(cart)
+  binding.pry
+  coupCart = apply_coupons(conCart, coupons)
+  binding.pry
+  apply_clearance(coupCart)
+  binding.pry
+  coupCart.each do |product|
+    total += product[:price] * product[:count]
+  end
+  if total > 100
+    binding.pry
+    total = total * 0.90
+  end
+  binding.pry
+  return total
+end
+
+
   # Consult README for inputs and outputs
   #
   # This method should call
@@ -22,4 +77,3 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-end
